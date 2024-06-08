99106dba9b8921f864aac1d98ba98e42086f6ab0
MoloZzz <mega_oleksii@ukr.net>
2024-06-08 15:58:05 +0300

    feat/ basic structure

---
+++ src/main/java/periodicals/PeriodicalsSpringBackendPlatformApplication.java
@@ -0,0 +1,13 @@
+package periodicals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodicalsSpringBackendPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeriodicalsSpringBackendPlatformApplication.class, args);
+    }
+
+}
