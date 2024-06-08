3aa7b1cc3d6b4183f285132cccd66cbe3c4c13c0
MoloZzz <mega_oleksii@ukr.net>
2024-06-08 19:06:12 +0300

    feat / publication controller

--- src/main/java/periodicals/controller/PublicationController.java
+++ src/main/java/periodicals/controller/PublicationController.java
@@ -1,8 +1,12 @@
 package periodicals.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import periodicals.model.Publication;
 import periodicals.service.PublicationService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/publications")
@@ -10,6 +14,28 @@ public class PublicationController {
     @Autowired
     private PublicationService publicationService;
 
-    // Endpoints
+    @PostMapping
+    public ResponseEntity<Publication> createPublication(@RequestBody Publication publication) {
+        Publication savedPublication = publicationService.savePublication(publication);
+        return ResponseEntity.ok(savedPublication);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Publication> updatePublication(@PathVariable Long id, @RequestBody Publication publication) {
+        Publication updatedPublication = publicationService.updatePublication(id, publication);
+        return ResponseEntity.ok(updatedPublication);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePublication(@PathVariable Long id) {
+        publicationService.deletePublication(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Publication>> getAllPublications() {
+        List<Publication> publications = publicationService.getAllPublications();
+        return ResponseEntity.ok(publications);
+    }
 }
 
--- src/main/java/periodicals/service/PublicationService.java
+++ src/main/java/periodicals/service/PublicationService.java
@@ -1,13 +1,42 @@
 package periodicals.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.server.ResponseStatusException;
+import periodicals.model.Publication;
 import periodicals.repository.PublicationRepository;
+
+import java.util.List;
 
 @Service
 public class PublicationService {
-    @Autowired
-    private PublicationRepository publicationRepository;
+    private final PublicationRepository publicationRepository;
 
-    // Business logic methods
+    public PublicationService(PublicationRepository publicationRepository) {
+        this.publicationRepository = publicationRepository;
+    }
+
+    public Publication savePublication(Publication publication) {
+        return publicationRepository.save(publication);
+    }
+
+    public Publication updatePublication(Long id, Publication publication) {
+        Publication existingPublication = publicationRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Publication not found"));
+        existingPublication.setTitle(publication.getTitle());
+        existingPublication.setDescription(publication.getDescription());
+        existingPublication.setPrice(publication.getPrice());
+        existingPublication.setIssueFrequency(publication.getIssueFrequency());
+        return publicationRepository.save(existingPublication);
+    }
+
+    public void deletePublication(Long id) {
+        publicationRepository.deleteById(id);
+    }
+
+    public List<Publication> getAllPublications() {
+        return publicationRepository.findAll();
+    }
 }
+
